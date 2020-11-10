f7b138f572e84660bfef7585bff069e9d3a20035
Agatha28 <agasantoso7@gmail.com>
2020-11-10 12:34:08 +0700

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React, {useState} from "react";
+import Friend from './Friend';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const [text, setText] = useState('react is great');
+const [friendList, setFriends] = useState(["Joe", "Rob", "Josh"]);
+
+function changeText() {
+  setText("react is super awesome")
+}
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to {text}.
         </p>
         <a
           className="App-link"
@@ -17,6 +26,10 @@ function App() {
         >
           Learn React
         </a>
+        {friendList.map(friend => {
+          return <Friend name={friend}/>
+        })}
+        <button onClick={changeText}>Change Text</button>
       </header>
     </div>
   );
---
+++ src/Friend.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Friend (props) {
+    return (
+        <div>
+            <h1>Friend: {props.name}</h1>
+        </div>
+    )
+}
+
+export default Friend;
